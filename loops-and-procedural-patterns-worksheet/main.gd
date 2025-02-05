3a633b14f77a4aa6846c17e2d46ffc41aab3d695
xKiniax <erin.rogalska@gmail.com>
2025-02-05 14:58:19 +0000

    Draw circle and draw line

--- loops-and-procedural-patterns-worksheet/main.gd
+++ loops-and-procedural-patterns-worksheet/main.gd
@@ -3,6 +3,10 @@ extends Node2D
 func _ready() -> void:
 	#loops - is a statement that allow your code to be repeated (similar to if statements but can repeat)
 	
+	_draw
+	
+	
+	
 	var  inc = 0
 	while inc < 10:
 		print("running a loop", inc)
@@ -19,3 +23,16 @@ func _ready() -> void:
 		
 func _process(delta: float) -> void:
 	pass
+
+var z = draw_line
+
+func _draw() -> void:
+	for z in range(10):
+		draw_line(Vector2(1.5, 1.0), Vector2(1.5, 4.0), Color.GREEN, 1.0)
+	
+	var cen = Vector2(100, 100)
+	var rad = 40
+	var col = Color(1,0,0)
+
+	for c in range(10):
+		draw_circle(cen,rad,col)
